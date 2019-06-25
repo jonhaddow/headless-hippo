1276e75273f47a3d37d601c0d2a45580650c14f1
Jon Haddow <haddowjon@protonmail.com>
2019-06-25 21:05:29 +0100

    Set default webpack mode to "production"

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
+	mode: 'production',
 	entry: ['./src/index.tsx'],
 	module: {
 		rules: [

8f221b403757b14c21615b84872132c9f905b68b
Jon Haddow <haddowjon@protonmail.com>
2019-07-21 21:39:35 +0100

    Add basic slider timer functionality

---
+++ src/home/slider/slider.css.d.ts
@@ -0,0 +1,5 @@
+export const sliderWrapper: string;
+export const sliderList: string;
+export const sliderListItem: string;
+export const activeSliderItem: string;
+export const postTitle: string;
--- src/home/slider/slider.tsx
+++ src/home/slider/slider.tsx
@@ -1,23 +1,56 @@
-import React, { Component } from "react";
+import React, { Component, CSSProperties } from "react";
 import ApiRequest, { URLS } from '../../models/api_request';
+import styles from './slider.css';
 
 interface RecentPost {
 	slug: string;
 	type: string;
 	title: string;
+	image: string;
+	active: boolean;
 }
 
 export default class Slider extends Component<{}, { recentPosts: RecentPost[] }> {
+	private sliderTimer: NodeJS.Timer;
+
 	public constructor(props: {}) {
 		super(props);
 
 		this.state = { recentPosts: [] };
+	}
 
-		this.initializeModel();
+	public async componentDidMount(): Promise<void> {
+		await this.initializeModel();
+		this.setupSliderTimer();
+	}
+
+	public componentWillUnmount(): void {
+		clearInterval(this.sliderTimer);
+	}
+	
+	private setupSliderTimer(): void {
+		this.sliderTimer = setInterval((): void => {
+			const { recentPosts } = this.state;
+			const currentIndex = recentPosts.findIndex((post): boolean => post.active);
+
+			let nextIndex = currentIndex + 1;
+			if (nextIndex == recentPosts.length) nextIndex = 0; 
+	
+			recentPosts.forEach((post, index): void => {
+				post.active = index === nextIndex;
+			});
+	
+			this.setState({
+				recentPosts: recentPosts
+			});
+		}, 4000);
 	}
 
 	private async initializeModel(): Promise<void> {
 		const recentPosts = await ApiRequest.fetch<RecentPost[]>(URLS.getRecentPosts());
+
+		recentPosts[0].active = true;
+		
 		this.setState({
 			recentPosts: recentPosts
 		});
@@ -27,19 +60,26 @@ export default class Slider extends Component<{}, { recentPosts: RecentPost[] }>
 
 		const { recentPosts } = this.state;
 
-		if (recentPosts == null) return null;
-		
+		if (recentPosts == null || recentPosts.length === 0) return null;
+
 		const sliderItems = recentPosts.map((post): JSX.Element => {
+			const style: CSSProperties = {
+				backgroundImage: `url("${post.image}")`
+			};
 			return(
-				<li key={post.slug}>
-					<span className='title'>{post.title}</span>
+				<li 
+					key={post.slug} 
+					style={style} 
+					className={`${styles.sliderListItem} ${post.active ? styles.activeSliderItem : '' }`}
+				>
+					<span className={styles.postTitle}>{post.title}</span>
 				</li>
 			)
 		});
 		
 		return(
-			<div className='slider'>
-				<ul>
+			<div className={styles.sliderWrapper}>
+				<ul className={styles.sliderList}>
 					{sliderItems}
 				</ul>
 			</div>
